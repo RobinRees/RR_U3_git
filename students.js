0b3e4c1cbe5166843a7e379c88a1832f15db8249
Robin Rees <97979531+RobinRees@users.noreply.github.com>
2022-03-24 11:11:18 +0100

    Changed search funktion

--- students.js
+++ students.js
@@ -1,14 +1,13 @@
 "use strict";
 
-let selectTheElement = (selectElement) => {
-    return document.querySelector(selectElement);
-}
+
 
 //key up funktion
 function onKeyUp() {
     console.log(this.value);
-    let foundStudent = DATABASE.students.filter((student) => student.lastName.includes(input.value))
+    let foundStudent = DATABASE.students.filter((student) => student.lastName.toLowerCase().includes(input.value))
     console.log(foundStudent)
+
 }
 let input = document.querySelector("input");
 input.addEventListener("keyup", onKeyUp);
